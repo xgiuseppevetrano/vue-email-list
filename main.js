2f7b10899627a7e9081d281d938d303c62f3dbb5
xgiuseppevetrano <xgiuseppevetrano@gmail.com>
2022-05-30 15:56:04 +0200

    add milestone 1

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
-// DESCRIZIONE
+// MILESTONE 1
 // Attraverso l'apposita API di Boolean: https://flynn.boolean.careers/exercises/api/random/mail
 // Generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
 
-// Bonus
+// BONUS
 // Far comparire gli indirizzi email solamente quando sono stati tutti generati.
 
+const app = new Vue ({
+    el: '#app',
+    data: {
+        mails: [],
+    },
+    created() {
+        for (let i = 0; i < 10; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((res) => {
+                this.mails.push(res.data.response);
+            })
+        }
+    }
+});
